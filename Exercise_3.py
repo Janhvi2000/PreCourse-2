489be3cb96b53dd7a53e8c05beadaf6e9ed67f2e
Janhvi Guha <87542822+Janhvi2000@users.noreply.github.com>
2025-07-28 00:17:11 -0700

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,36 @@
+# Time complexity - O(n)
+# Space complexity - O(1)
+
 # Node class  
 class Node:  
-  
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data, next = None):  
+        self.data = data
+        self.next = next
         
 class LinkedList: 
-  
     def __init__(self): 
-        
-  
+        self.start = None
+    
+    # initializing new node to add to linked list
     def push(self, new_data): 
-        
+        new = Node(new_data)
+        new.next = self.start
+        self.start = new
   
-    # Function to get the middle of  
-    # the linked list 
+    # Function to get the middle of the linked list 
+    # using slow and fast pointer to traverse the list, since fast travels at 2 times slow pointer, when fast reaches end of list, slow will reach the middle
     def printMiddle(self): 
-
+        slow = self.start
+        fast = self.start
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        if slow: print("Middle element is:", slow.data)
+        
+        
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
