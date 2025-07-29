c2a931b27f801be398cda3d4dbf581bdb9a02ad5
Janhvi Guha <87542822+Janhvi2000@users.noreply.github.com>
2025-07-29 01:39:24 -0700

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,18 +1,52 @@
-# Python program for implementation of MergeSort 
+# Time complexity - O(n log n)
+# Space complexity - O(n)
+# Code runs properly
+
+# Approach:
+# Merge sort is divide and conquer algorithm.
+# Array is recursively divided into two halves until subarrays has size of one.
+# Then, during merge step, two sorted halves are combined into single sorted array.
+# Continue until entire array is merged and sorted.
+
 def mergeSort(arr):
-  
-  #write your code here
-  
-# Code to print the list 
-def printList(arr): 
-    
-    #write your code here
-  
-# driver code to test the above code 
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+def printList(arr):
+    for i in arr:
+        print(i, end=" ")
+    print()
+
 if __name__ == '__main__': 
     arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end="\n") 
-    printList(arr) 
+    print("Given array is:")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is:")
+    printList(arr)
